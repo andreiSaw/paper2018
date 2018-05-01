68ef8d48635608f4f8a7cd4a4f7b88c021f07591
macbook <isaevnextdoor@gmail.com>
2018-05-01 14:27:12 +0300

    montecarlo

---
+++ images/mc/run.py
@@ -0,0 +1,41 @@
+import random
+from multiprocessing import Pool
+
+import sys
+
+import time
+
+np = int(sys.argv[1])
+temps1 = time.time()
+
+
+def count_pt(N):
+    count_in = 0
+    for i in range(N):
+        x = random.uniform(-1.0, 1.0)
+        y = random.uniform(-1.0, 1.0)
+        # if it is within the unit circle
+        if x * x + y * y <= 1:
+            count_in += 1
+    return count_in
+
+
+if __name__ == '__main__':
+    print('You have {0:1d} CPUs'.format(np))
+
+    # Nummber of points to use for the Pi estimation
+    n = 10000000
+
+    # iterable with a list of points to generate in each worker
+    # each worker process gets n/np number of points to calculate Pi from
+
+    part_count = [(int)(n / np)] * np
+
+    # Create the worker pool
+    # http://docs.python.org/library/multiprocessing.html#module-multiprocessing.pool
+    pool = Pool(processes=np)
+
+    # parallel map
+    count = pool.map(count_pt, part_count)
+    print("Esitmated value of Pi:: ", sum(count) / (n * 1.0) * 4)
+    print(time.time() - temps1)
