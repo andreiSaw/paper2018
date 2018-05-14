05b4eb59f26716bfd9263f6405a0c5cf550a8127
macbook <isaevnextdoor@gmail.com>
2018-05-14 15:29:50 +0300

    sleep docker

---
+++ images/sleep/README.md
@@ -0,0 +1,7 @@
+Sleep task
+--
+1. Embarrassingly parallel problem;
+2. To build an image use `docker build -t sleep .`;
+3. To run it use `docker run sleep python3 run.py 100 50`,
+if you'd like to run simulation of 100 secs sleep on 50 CPUs;
+4. Computation time will be calculated by completion.
---
+++ images/sleep/run.py
@@ -0,0 +1,22 @@
+import sys
+import time
+from multiprocessing import Pool
+
+durarion = int(sys.argv[1])
+np = int(sys.argv[2])
+
+def sleep2sec(durarion):
+    time.sleep(durarion)
+
+
+if __name__ == '__main__':
+    part_count = [int(durarion / np)] * np
+
+    # Create the worker pool
+    # http://docs.python.org/library/multiprocessing.html#module-multiprocessing.pool
+    pool = Pool(processes=np)
+
+    temps1 = time.time()
+    # parallel map
+    count = pool.map(sleep2sec, part_count)
+    print(time.time() - temps1)
