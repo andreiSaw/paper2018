1c18c8ddeac88bea65c3d1cc06404552b67797e8
macbook <isaevnextdoor@gmail.com>
2018-05-29 15:56:37 +0300

    x

--- images/xgb/run.py
+++ images/xgb/run.py
@@ -6,7 +6,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from xgboost import XGBClassifier
 
-n_estimators = int(sys.argv[1])
+nthread = int(sys.argv[1])
 warnings.filterwarnings(action='ignore', category=DeprecationWarning)
 
 
@@ -26,9 +26,10 @@ def main():
     y = df['target']
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    # print(len(X_train))
 
     # set xgboost params
-    clf = XGBClassifier(n_estimators=n_estimators)
+    clf = XGBClassifier(nthread=nthread)
 
     temps1 = time.time()
     clf.fit(X_train, y_train)
