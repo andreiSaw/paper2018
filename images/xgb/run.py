9a54c25295b812bc4a257d15d2c041d1cc7e2db3
macbook <isaevnextdoor@gmail.com>
2018-06-07 21:25:12 +0300

    letter

--- images/xgb/run.py
+++ images/xgb/run.py
@@ -8,22 +8,25 @@ from xgboost import XGBClassifier
 
 nthread = int(sys.argv[1])
 warnings.filterwarnings(action='ignore', category=DeprecationWarning)
+# nthread = 16
 
 
 def main():
-    df = pd.read_csv("/input/images/xgb/nursery.data.txt", header=None)  # dataset loading
-    df.columns = ['parents', 'has_nurs', 'form', 'children', 'housing', 'finance', 'social', 'health', 'target']
+    df = pd.read_csv("/input/images/xgb/dataset.csv", header=None)  # dataset loading
+    # df = pd.read_csv("dataset.csv", header=None)  # dataset loading
+
+    # df.columns = ['parents', 'has_nurs', 'form', 'children', 'housing', 'finance', 'social', 'health', 'target']
     from sklearn import preprocessing
-    def transform(feature):
-        le = preprocessing.LabelEncoder()
-        le.fit(df[feature])
-        df[feature] = le.transform(df[feature])
-
-    for feature in df.columns:
-        transform(feature)
-
-    X = df.drop(['target'], axis=1)
-    y = df['target']
+    # def transform(feature):
+    #     le = preprocessing.LabelEncoder()
+    #     le.fit(df[feature])
+    #     df[feature] = le.transform(df[feature])
+    #
+    # for feature in df.columns:
+    #     transform(feature)
+    # print(df.columns)
+    X = df.drop([0], axis=1)
+    y = df[0]
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     # print(len(X_train))
